9f298f577384b203249aeb1b42cb3e91f21ae3a3
andryansyhh <andryansyhh@gmail.com>
2021-05-31 21:11:56 +0700

    masih error untuk crud nya

--- API/entity/user.go
+++ API/entity/user.go
@@ -15,6 +15,30 @@ type User struct {
 	Phone     uint           `json:"phone"`
 	Address   string         `json:"adress"`
 	CreatedAt time.Time      `json:"created_at"`
-	UpdateAt  time.Time      `json:"update_at"`
+	UpdatedAt time.Time      `json:"update_at"`
 	DeletedAt gorm.DeletedAt `json:"-"`
+}
+
+type UserInput struct {
+	FirstName string `json:"first_name" binding:"required"`
+	LastName  string `json:"last_name" binding:"required"`
+	Email     string `json:"email" binding:"required,email"`
+	Username  string `json:"username" binding:"required"`
+	Password  string `json:"password" binding:"required"`
+	Phone     uint   `json:"phone" binding:"required"`
+	Address   string `json:"address" binding:"required"`
+}
+
+type UpdateUserInput struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Username  string `json:"username"`
+	Phone     uint   `json:"phone"`
+	Address   string `json:"address"`
+}
+
+type LoginUserInput struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
 }
--- API/main.go
+++ API/main.go
@@ -1,5 +1,32 @@
 package main
 
-func ()  {
-	
+import (
+	"github.com/gin-gonic/gin"
+	"go-bake/auth"
+	"go-bake/config"
+	"go-bake/handler"
+	"go-bake/user"
+	"gorm.io/gorm"
+)
+
+var (
+	DB             *gorm.DB = config.SetupConnection()
+	userRepository          = user.NewRepository(DB)
+	userService             = user.NewService(userRepository)
+	authService             = auth.NewService()
+	userHandler             = handler.NewUserHandler(userService, authService)
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/users", handler.MiddleWare(userService, authService), userHandler.ShowUserHandler)
+	r.GET("/users/:user_id", handler.MiddleWare(userService, authService), userHandler.GetUserByIDHandler)
+	r.POST("/users/register", userHandler.CreateUserHandler)
+	r.POST("/users/login", userHandler.LoginUserHandler)
+	r.PUT("/users/:user_id", handler.MiddleWare(userService, authService), userHandler.UpdateUserByIDHandler)
+	r.DELETE("/users/:user_id", handler.MiddleWare(userService, authService), userHandler.DeleteUserByIDHandler)
+
+	r.Run(":1204")
+
 }
