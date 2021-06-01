db9d327765e98745b9baaedbca5470832da5abdc
andryansyhh <andryansyhh@gmail.com>
2021-06-02 00:02:19 +0700

    done handle register & login

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -1,21 +1,52 @@
-import React from 'react'
+import Axios from 'axios'
+import React, { useState } from 'react'
+import swal from 'sweetalert'
+import { API_URL, resetForm } from '../../utils/utils'
 
 function Login() {
+
+    const [inputEmail, setEmail] = useState("")
+    const [inputPw, setPw] = useState("")
+
+    const handleSubmit = (e) => {
+        
+        e.preventDefault()
+
+        const data = {
+            email : inputEmail,
+            password : inputPw
+        }
+
+        console.log(data);
+
+        Axios.post(`${API_URL}/users/login`, data)
+            .then((res)=>{
+
+                if (res.data.Data.token) {
+                    localStorage.setItem("user", JSON.stringify(res.data.Data))
+                }
+                swal("Resgister success!", "", "success");
+                resetForm("login-form")
+                console.log(res)
+            })
+    }
+
     return (
         <div className="login-wrapper">
             <div className="login">
-                <form id="login-form">
+                <form onSubmit={handleSubmit} id="login-form">
                     <h2>
                         Login
                     </h2>
 
                     <div className="input-container">
                     <label htmlFor="email">Email</label>
-                    <input type="text" name="email" id="email" />
+                    <input onChange={(e) => setEmail(e.target.value)} type="text" name="email" id="email" />
                 </div>
+
                 <div className="input-container">
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" />
+                    <input onChange={(e) => setPw(e.target.value) } type="password" name="password" id="password" />
                 </div>
                 <div className="btn-login">
                     <button>Log in</button>
--- client/src/pages/register/register.jsx
+++ client/src/pages/register/register.jsx
@@ -1,47 +1,117 @@
-import React from 'react'
+import React, { useState } from "react";
+import Axios from "axios";
+import { API_URL, resetForm } from "../../utils/utils";
+import swal from "sweetalert";
 
 function Register() {
-    return (
-        <div className="regis-wrapper">
+  const [inputFirst, setInputFirst] = useState("");
+  const [inputLast, setInputLast] = useState("");
+  const [inputUsername, setInputUsername] = useState("");
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
+  const [inputPhone, setInputPhone] = useState("");
 
-            <div className="regis">
-                <form id="regis-form">
-                <h2>
-                    Create new account
-                </h2>
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-                <div className="input-container">
-                    <label htmlFor="firstname">First name</label>
-                    <input type="text" name="firstname" id="firstname" />
-                </div>
-                <div className="input-container">
-                    <label htmlFor="lastname">Last Name</label>
-                    <input type="text" name="lastname" id="lastname" />
-                </div>
-                <div className="input-container">
-                    <label htmlFor="username">Username</label>
-                    <input type="text" name="username" id="username" />
-                </div>
-                <div className="input-container">
-                    <label htmlFor="email">Email</label>
-                    <input type="text" name="name" id="name" />
-                </div>
-                <div className="input-container">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" />
-                </div>
-                <div className="input-container">
-                    <label htmlFor="phone">Phone</label>
-                    <input type="number" name="phone" id="phone" />
-                </div>
-                <div className="btn-regis">
-                    <button>Sign up</button>
-                </div>
-                </form>
-            </div>
-            
-        </div>
-    )
+    const data = {
+      first_name: inputFirst,
+      last_name: inputLast,
+      email: inputEmail,
+      username: inputUsername,
+      password: inputPassword,
+      phone: parseInt(inputPhone),
+      address: "-",
+    };
+
+    console.log(data);
+
+    Axios.post(`${API_URL}/users/register`, data)
+      .then((res) => {
+        swal("Resgister success!", "", "success");
+        resetForm("regis-form")
+        console.log(res);
+      })
+      .catch((err) => console.log(err.message));
+  };
+
+  
+  
+  
+
+  return (
+    <div className="regis-wrapper">
+      <div className="regis">
+        <form onSubmit={handleSubmit} id="regis-form">
+          <h2>Create new account</h2>
+
+          <div className="input-container">
+            <label htmlFor="firstname">First name</label>
+            <input
+              onChange={(e) => setInputFirst(e.target.value)}
+              type="text"
+              name="firstname"
+              id="firstname"
+            />
+          </div>
+
+          <div className="input-container">
+            <label htmlFor="lastname">Last Name</label>
+            <input
+              onChange={(e) => setInputLast(e.target.value)}
+              type="text"
+              name="lastname"
+              id="lastname"
+            />
+          </div>
+
+          <div className="input-container">
+            <label htmlFor="username">Username</label>
+            <input
+              onChange={(e) => setInputUsername(e.target.value)}
+              type="text"
+              name="username"
+              id="username"
+            />
+          </div>
+
+          <div className="input-container">
+            <label htmlFor="email">Email</label>
+            <input
+              onChange={(e) => setInputEmail(e.target.value)}
+              type="text"
+              name="email"
+              id="email"
+            />
+          </div>
+
+          <div className="input-container">
+            <label htmlFor="password">Password</label>
+            <input
+              onChange={(e) => setInputPassword(e.target.value)}
+              type="password"
+              name="password"
+              id="password"
+            />
+          </div>
+
+          <div className="input-container">
+            <label htmlFor="phone">Phone</label>
+            <input
+              onChange={(e) => setInputPhone(e.target.value)}
+              type="number"
+              name="phone"
+              id="phone"
+            />
+          </div>
+
+          <div className="btn-regis">
+            <button>Sign up</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
